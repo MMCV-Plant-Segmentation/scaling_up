7adb685c59443f0ab9097fa592f3c88025725fb7
etwhhb <etwilbur22@gmail.com>
2025-02-06 19:23:04 -0600

    clone video and ffmpeg attempt

--- pointify.sh
+++ pointify.sh
@@ -1,3 +1,6 @@
 mkdir -p ~/.config/rclone
 cp /secrets/rclone.conf ~/.config/rclone
+mkdir /pvc/raw
+rclone copy nautilus:my-bucket/my-directory /pvc
+ffmpeg -i /pvc/*.MOV -vf fps=2 /pvc/raw/24r_test_frame%06d.png
 sleep infinity
