446ac0e9c6b6c943ffb5cf9e5a8c8b1f3d293695
etwhhb <etwilbur22@gmail.com>
2025-02-11 18:38:47 -0600

    GPU selection, initContainer, ffmpeg -hwaccel, rclone to S3

--- pointify.sh
+++ pointify.sh
@@ -1,6 +1,8 @@
 mkdir -p ~/.config/rclone
 cp /secrets/rclone.conf ~/.config/rclone
-mkdir /pvc/raw
+mkdir /pvc/frames
 rclone copy nautilus:my-bucket/my-directory /pvc
-ffmpeg -i /pvc/*.MOV -vf fps=2 /pvc/raw/24r_test_frame%06d.png
+ffmpeg -hwaccel cuda -i /pvc/*.MOV -vf fps=2 /pvc/frames/24r_test_frame%06d.png
+zip /pvc/frames.zip /pvc/frames
+rclone copy /pvc/frames.zip nautilus:my-bucket
 sleep infinity
