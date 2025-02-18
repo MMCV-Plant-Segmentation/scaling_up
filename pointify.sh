a625a4ae8fdea86da3f00e5a2aafb8c9b588f15e
etwhhb <etwilbur22@gmail.com>
2025-02-18 17:46:02 -0600

    added comments, tested ffmpeg command with hwaccel, 18.2.2025

--- pointify.sh
+++ pointify.sh
@@ -1,8 +1,24 @@
+# this is ../e/pointillist_maize/code/scaling_up/pointify.sh
+#
 mkdir -p ~/.config/rclone
 cp /secrets/rclone.conf ~/.config/rclone
+#
+# make the persistent volume claim
+#
 mkdir /pvc/frames
 rclone copy nautilus:my-bucket/my-directory /pvc
-ffmpeg -i /pvc/*.MOV -vf fps=2 /pvc/frames/24r_test_frame%06d.png
+#
+# get the cuda ffmpeg needs
+#
+# export LD_LIBRARY_PATH="/usr/local/cuda-12.8/compat:$LD_LIBRARY_PATH"
+# apt install libnvidia-decode-550-server
+#
+# run ffmpeg on whatever is in the directory and output the frames
+#
+ffmpeg -hwaccel cuda -i /pvc/*.MOV -vf fps=2 /pvc/frames/24r_test_frame%06d.png
+#
+# move the output to safety and sleep
+#
 zip /pvc/frames.zip /pvc/frames
 rclone copy /pvc/frames.zip nautilus:my-bucket
 sleep infinity
