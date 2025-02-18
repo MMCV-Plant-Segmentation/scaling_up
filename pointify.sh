b76814455db5087631ab04b513a916279136dda8
etwhhb <etwilbur22@gmail.com>
2025-02-18 16:29:11 -0600

    took out cuda part of ffmpeg command

--- pointify.sh
+++ pointify.sh
@@ -2,7 +2,7 @@ mkdir -p ~/.config/rclone
 cp /secrets/rclone.conf ~/.config/rclone
 mkdir /pvc/frames
 rclone copy nautilus:my-bucket/my-directory /pvc
-ffmpeg -hwaccel cuda -i /pvc/*.MOV -vf fps=2 /pvc/frames/24r_test_frame%06d.png
+ffmpeg -i /pvc/*.MOV -vf fps=2 /pvc/frames/24r_test_frame%06d.png
 zip /pvc/frames.zip /pvc/frames
 rclone copy /pvc/frames.zip nautilus:my-bucket
 sleep infinity
