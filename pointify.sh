16011a0cac886183b5ee5006e98efce53adcd78f
etwhhb <etwilbur22@gmail.com>
2025-02-27 16:57:15 -0600

    7z

--- pointify.sh
+++ pointify.sh
@@ -19,6 +19,6 @@ ffmpeg -threads 0 -i /pvc/*.MOV -vf fps=2 /pvc/frames/24r_test_frame%06d.png
 #
 # move the output to safety and sleep
 #
-zip /pvc/frames.zip /pvc/frames/*
-rclone copy --progress /pvc/frames.zip nautilus:my-bucket
+7z a -mmt /pvc/frames.zip /pvc/frames/*
+rclone copy --progress /pvc/frames.7z nautilus:my-bucket
 sleep infinity
