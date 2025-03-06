98371e33dc5e335fb648a98ad4c5987760c7c18c
etwhhb <etwilbur22@gmail.com>
2025-03-06 16:51:39 -0600

    Added gpu flags to colmap command

--- reconstruct.sh
+++ reconstruct.sh
@@ -6,6 +6,6 @@ cp /secrets/rclone.conf ~/.config/rclone
 # make the persistent volume claim
 #
 #
-colmap automatic_reconstructor --image_path /pvc/frames --workspace_path /pvc/colmapresult --data_type video --quality extreme
+colmap automatic_reconstructor --image_path /pvc/frames --workspace_path /pvc/colmapresult --data_type video --quality extreme --SiftExtraction.use_gpu 1 --SiftMatching.use_gpu 1
 echo colmap did its thing maybe
 sleep infinity
