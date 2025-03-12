ed53daaac3e1847775afc9a219fb276ac53b52c8
etwhhb <etwilbur22@gmail.com>
2025-03-11 19:42:43 -0500

    Changed tar command

--- reconstruct.sh
+++ reconstruct.sh
@@ -7,7 +7,7 @@ mkdir colmapresult
 #Get frames from S3 storage
 rclone copy nautilus:my-bucket/frames.tar /home/ubuntu
 #extract tar
-tar xf /home/ubuntu/frames.tar --directory /home/ubuntu
+tar xf /home/ubuntu/frames.tar 
 #run colmap
 colmap automatic_reconstructor --image_path /home/ubuntu/frames --workspace_path /home/ubuntu/colmapresult --data_type video --quality extreme --num_threads=18
 echo colmap did its thing maybe
