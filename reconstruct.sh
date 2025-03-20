1ed634ef0824c41ae978823bb1574a6bee547b63
creallfluharty <creallfluharty@gmail.com>
2025-03-19 19:39:33 -0500

    display tar extraction progress

--- reconstruct.sh
+++ reconstruct.sh
@@ -7,6 +7,6 @@ mkdir colmapresult
 #Get frames from S3 storage
 rclone copy --progress nautilus:my-bucket/frames.tar /home/ubuntu
 #extract tar
-tar xf /home/ubuntu/frames.tar
+tar xvf /home/ubuntu/frames.tar
 #run colmap
 colmap automatic_reconstructor --image_path /home/ubuntu/frames --workspace_path /home/ubuntu/colmapresult --data_type video --quality extreme --num_threads=18
