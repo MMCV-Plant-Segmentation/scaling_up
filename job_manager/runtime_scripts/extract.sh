680f82ffdfc79999b4055ba0ced5a7cdcdbc4a0b
creallfluharty <creallfluharty@gmail.com>
2025-06-26 16:24:13 -0500

    fix tar file name maybe

--- job_manager/runtime_scripts/extract.sh
+++ job_manager/runtime_scripts/extract.sh
@@ -8,5 +8,5 @@ rclone copy --progress "nautilus:my-bucket/my-directory/$VIDEO_FILE_NAME" /home/
 
 ffmpeg -threads 0 -progress - -i "/home/ubuntu/$VIDEO_FILE_NAME" -vf fps=2 "/home/ubuntu/frames/$VIDEO_FILE_NAME%06d.png"
 
-tar -cvf frames.tar frames/*
+tar -cvf $VIDEO_FILE_NAME-frames.tar frames/*
 rclone copy --progress /home/ubuntu/$VIDEO_FILE_NAME-frames.tar nautilus:my-bucket
