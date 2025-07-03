d086c3075e12d83fad0b9af4a4cead3406775931
creallfluharty <creallfluharty@gmail.com>
2025-07-03 16:05:43 -0500

    remove extension name

--- job_manager/runtime_scripts/extract.sh
+++ job_manager/runtime_scripts/extract.sh
@@ -2,6 +2,7 @@ VIDEO_BUCKET_NAME=$1
 VIDEO_FILE_PATH=$2
 VIDEO_FOLDER_PATH="$(dirname $VIDEO_FILE_PATH)"
 VIDEO_FILE_NAME="$(basename $VIDEO_FILE_PATH)"
+VIDEO_NAME="${VIDEO_FILE_NAME%.*}"
 
 mkdir -p ~/.config/rclone
 cp /secrets/rclone.conf ~/.config/rclone
@@ -9,7 +10,7 @@ cp /secrets/rclone.conf ~/.config/rclone
 mkdir frames
 rclone copy --progress "nautilus:$VIDEO_BUCKET/$VIDEO_FILE_PATH" .
 
-ffmpeg -threads 0 -progress - -i "$VIDEO_FILE_NAME" -vf fps=2 "frames/$VIDEO_FILE_NAME%06d.png"
+ffmpeg -threads 0 -progress - -i "$VIDEO_FILE_NAME" -vf fps=2 "frames/$VIDEO_NAME%06d.png"
 
-tar -cvf "$VIDEO_FILE_NAME-frames.tar" frames/*
-rclone copy --progress "$VIDEO_FILE_NAME-frames.tar" "nautilus:$VIDEO_BUCKET/$VIDEO_FOLDER_PATH"
+tar -cvf "$VIDEO_NAME-frames.tar" frames/*
+rclone copy --progress "$VIDEO_NAME-frames.tar" "nautilus:$VIDEO_BUCKET/$VIDEO_FOLDER_PATH"
